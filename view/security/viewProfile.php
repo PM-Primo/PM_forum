298fb72775da490a626d28f10a9b1141e0d76530
PM-Primo <pierrem.primo@gmail.com>
2022-11-15 14:51:10 +0100

    added nb of posts & topics in user profile

--- view/security/viewProfile.php
+++ view/security/viewProfile.php
@@ -4,6 +4,31 @@ $user = $result["data"]['user'];
 
 <h1>Profil de <?=$user->getPseudoUser()?></h1>
 
-<p>Pseudo : <?=$user->getPseudoUser()?></p>
-<p>Adresse mail : <?=$user->getEmailUser()?></p>
-<p>Rôle : <?=$user->getRoleUser()?></p>
+<div class="detailsProfil">
+    <p>Pseudo : <?=$user->getPseudoUser()?></p>
+    <p>Adresse mail : <?=$user->getEmailUser()?></p>
+    <p>Rôle : <?=$user->getRoleUser()?></p>
+    <?php 
+    if($user->getNbTopicsUser()>1){
+        $strTopics = $user->getNbTopicsUser()." topics créés";
+    }
+    else if($user->getNbTopicsUser()==1){
+        $strTopics = "1 topic créé";
+    }
+    else{
+        $strTopics = "Aucun topic créé";
+    }
+
+    if($user->getNbPostsUser()>1){
+        $strPosts = $user->getNbPostsUser()." messages rédigés";
+    }
+    else if($user->getNbPostsUser()==1){
+        $strPosts = "1 message rédigé";
+    }
+    else{
+        $strPosts = "Aucun message rédigé";
+    }
+    ?>
+    <p><?=$strTopics?></p>
+    <p><?=$strPosts?></p>
+</div>
