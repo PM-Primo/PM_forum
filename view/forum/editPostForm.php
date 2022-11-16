c4fa5dafff22f114cfea474937cc3f4e4176e53c
PM-Primo <pierrem.primo@gmail.com>
2022-11-16 09:23:36 +0100

    updated CSS for Post Edition pages

--- view/forum/editPostForm.php
+++ view/forum/editPostForm.php
@@ -7,14 +7,16 @@ $textePost = $post->getTextePost();
 
 ?>
 
-<form action="index.php?ctrl=forum&action=editPost&id=<?=$postId?>" method="post" class="formulaire">
-            Message :<br>
-            <textarea name="nvTextePost" rows="5" cols="45" required><?php echo $textePost; ?></textarea>        
-        </label>
-    </p>
+<div class="formWrapper">
+    <form action="index.php?ctrl=forum&action=editPost&id=<?=$postId?>" method="post" class="formulaireNvPost">
+                Message :<br>
+                <textarea name="nvTextePost" rows="5" cols="45" class="champTxtPost" required ><?php echo $textePost; ?></textarea>        
+            </label>
+        </p>
 
-    <div class="submit_wrapper">
-        <input type="submit" name="submit" value="Valider" class="submit">
-    </div>
-        
-</form>
+        <div class="submitWrapper">
+            <input type="submit" name="submit" value="Valider" class="submit">
+        </div>
+            
+    </form>
+</div>
